148d9e10a9e12fd0dd0e0d7e322fb1c2ba80f838
Tuomas Virtanen <katajakasa@gmail.com>
2014-08-30 06:06:17 +0300

    Exact string match.

--- src/query.php
+++ src/query.php
@@ -19,7 +19,7 @@ if(isset($_GET['param'])) {
         WHERE
             strings.file_id = files.id AND
             tags.string_id = strings.id AND
-            tags.tag LIKE ?");
+            tags.tag = ?");
     $s = "%".$_GET['param']."%";
     $q->bindValue(1, $s, PDO::PARAM_STR);
     $q->execute();
