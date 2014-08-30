4862628d615f4ab56bda576e471b39e769087716
Tuomas Virtanen <katajakasa@gmail.com>
2014-08-30 05:47:04 +0300

    Better tag search

--- src/query.php
+++ src/query.php
@@ -5,20 +5,21 @@ ini_set("display_errors", 1);
 if(isset($_GET['param'])) {
     $db = new PDO('sqlite:strings.db');
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $db->query("PRAGMA case_sensitive_like = ON");
+    //$db->query("PRAGMA case_sensitive_like = ON");
     
     $q = $db->prepare("
         SELECT 
             strings.str, 
             strings.anim_id, 
-            strings.type, 
+            strings.type,
             files.name AS filename,
             files.type AS filetype
         FROM 
-            strings,files
+            strings,files,tags
         WHERE
-            strings.file_id = files.id AND 
-            strings.str LIKE ?");
+            strings.file_id = files.id AND
+            tags.string_id = strings.id AND
+            tags.key LIKE ?");
     $s = "%".$_GET['param']."%";
     $q->bindValue(1, $s, PDO::PARAM_STR);
     $q->execute();
